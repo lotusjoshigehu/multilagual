d0afa3c70eeb34b6ec0aca8de0686c16a8bd69da
lotusjoshigehu <joshikamal626@gmail.com>
2026-04-25 13:54:11 +0530

    Update dbconnection.js

--- connection/dbconnection.js
+++ connection/dbconnection.js
@@ -2,21 +2,26 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "mysql",
+    
     dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-    }
-});
+    },
 
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.log("DB Error:", error);
-    }
-})();
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+
+    retry: {
+        max: 5
+    },
+
+    logging: false
+});
 
 module.exports = sequelize;
