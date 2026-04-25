9f384463ba0f5dc911f4973510ada35fb87eb86a
lotusjoshigehu <joshikamal626@gmail.com>
2026-04-25 14:36:08 +0530

    Change database dialect from MySQL to PostgreSQL

--- connection/dbconnection.js
+++ connection/dbconnection.js
@@ -1,27 +1,15 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "mysql",
-    
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
     dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-    },
-
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-
-    retry: {
-        max: 5
-    },
-
-    logging: false
+    }
 });
 
 module.exports = sequelize;
