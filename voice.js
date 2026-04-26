70f8845a2e4eaca8de70f0c323ca99ed741abc37
lotusjoshigehu <joshikamal626@gmail.com>
2026-04-26 17:04:33 +0530

    fix issue

--- voice.js
+++ voice.js
@@ -187,8 +187,21 @@ async function startCall() {
     document.getElementById("callControls").style.display = "block";
 
     peer = new RTCPeerConnection({
-        iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-    });
+    iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+
+        {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        },
+        {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        }
+    ]
+});
 
     localStream.getTracks().forEach(track =>
         peer.addTrack(track, localStream)
@@ -259,8 +272,21 @@ async function acceptCall() {
     targetSocketId = incomingFrom;
 
     peer = new RTCPeerConnection({
-        iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-    });
+    iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+
+        {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        },
+        {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject"
+        }
+    ]
+});
 
     localStream.getTracks().forEach(track =>
         peer.addTrack(track, localStream)
